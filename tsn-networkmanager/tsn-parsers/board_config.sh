57a7f4185c32c7cd4545047f7f2ccc6bdd1a74bc
Sagar Karmarkar <sagar.karmarkar@amd.com>
2023-04-25 14:35:46 -0700

    tsn-networkmanager: tsn-parsers: Script changes
    - Dynamically fetched the scriptpath
    - Added use of $sudo to source the script
    
    Signed-off-by: Sagar Karmarkar <sagar.karmarkar@amd.com>

--- tsn-networkmanager/tsn-parsers/board_config.sh
+++ tsn-networkmanager/tsn-parsers/board_config.sh
@@ -4,10 +4,7 @@
 ################################################################
 
 ip=192.168.1.40
-epmac=00:0a:35:00:01:20
-eth0mac=00:0a:35:00:01:2e
-eth1mac=00:0a:35:00:01:2f
+cwd="$( cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 ; pwd -P )"
 conffile="/usr/bin/ptp4l_master.conf"
-ptp_interface=eth0
-datadir="/home/root/datadir"
-parserdir="/home/root/parserdir"
+datadir="/home/ubuntu/datadir"
+parserdir=${cwd}
--- tsn-networkmanager/tsn-parsers/xnm.sh
+++ tsn-networkmanager/tsn-parsers/xnm.sh
@@ -1,42 +1,49 @@
+#!/bin/bash
 ################################################################
 # Copyright (c) 2022-2023 Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: MIT
 ################################################################
 
+cwd="$( cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 ; pwd -P )"
+
 launch_parser (){
 	echo $1
 	if [[ $1 == *"qbv"* ]]; then
-                python $parserdir/qbv-parser.py $1 &
+                $SUDO python3 $parserdir/qbv-parser.py $1 &
         fi
         if [[ $1 == *"fdb"* ]]; then
-                python $parserdir/fdb_parser.py $1 &
+                $SUDO python3 $parserdir/fdb_parser.py $1 &
         fi
         if [[ $1 == *"frer"* ]]; then
-                python $parserdir/frer_parser.py $1 &
+                $SUDO python3 $parserdir/frer_parser.py $1 &
         fi
         if [[ $1 == *"talker_status"* ]]; then
-                python $parserdir/ipic-parser.py $1 &
+                $SUDO python3 $parserdir/ipic-parser.py $1 &
         fi
         if [[ $1 == *"listener_status"* ]]; then
-                python $parserdir/ep_vlan.py $1 &
+                $SUDO python3 $parserdir/ep_vlan.py $1 &
         fi
 }
-cwd=$(pwd)
-config_file="$cwd/board_config.sh"
-datadir="/home/root/"
-parserdir="/home/root/"
-conffile="/usr/sbin/ptp4l_slave.conf"
-epmac=00:0a:35:00:01:10
-eth1mac=00:0a:35:00:01:1e
-eth2mac=00:0a:35:00:01:1f
+
+if [ "$EUID" -ne 0 ]
+	then echo "Please enter the password for sudo access"
+	SUDO=sudo
+else
+	SUDO=
+fi
+
+config_file="${cwd}/board_config.sh"
+conffile="/usr/bin/ptp4l_master.conf"
+datadir="/home/ubuntu/datadir"
+parserdir=${cwd}
 ip=192.168.1.99
-ptp_interface=eth1
-ptp_clock="/dev/ptp0"
+
 if [ ! -f $config_file ]; then
         echo "$config_file not found, proceeding with default configuration"
 else
         source $config_file
 fi
+
 if [ ! -d $parserdir ]; then
         echo "$parserdir not found"
         exit
@@ -46,7 +53,7 @@ if [ ! -d $datadir ]; then
         mkdir $datadir
 fi
 
-#Setup tftp server
+# Setup tftp server
 inetd_conf="/etc/inetd.conf"
 tftp_server_conf="69 dgram udp nowait root tftpd tftpd -c -l $datadir"
 
@@ -62,48 +69,50 @@ inetd
 
 # Obtain TSN MAC interface
 FND_EMAC=($(find /sys/devices/platform/ -iname  *tsn_emac_* | grep -i endpoint))
-
 EMAC0=`echo $(ls ${FND_EMAC[0]}/net/)`
 echo $EMAC0
 EMAC1=`echo $(ls ${FND_EMAC[1]}/net/)`
 echo $EMAC1
-ifconfig ep $ip up
-ifconfig $EMAC0 up
-ifconfig $EMAC1 up
+$SUDO ifconfig ep $ip up
+$SUDO ifconfig $EMAC0 up
+$SUDO ifconfig $EMAC1 up
 sleep 5
-for i in ep $EMAC0 $EMAC1
+for i in ep $EMAC0
 do
        if grep -q "down" /sys/class/net/$i/operstate > /dev/null
        then
-               echo "$i is not up"
+               echo "$i is not up, failed to setup PTP"
+               exit
        fi
 done
-switch_prog pst -s swp0 -t 4
-switch_prog pst -s swp1 -t 4
-switch_prog pst -s swp2 -t 4
+$SUDO switch_prog pst -s swp0 -t 4
+$SUDO switch_prog pst -s swp1 -t 4
+$SUDO switch_prog pst -s swp2 -t 4
 for i in swp0 swp1 swp2
 do
-       tmp=`switch_prog pst -g $i | grep "Forwarding State"`
+       tmp=`$SUDO switch_prog pst -g $i | grep "Forwarding State"`
        if [ "$tmp" == "" ]; then
                echo "Switch $i not set in forwarding state"
+               exit
        fi
 done
 
 if [ -f .ptplog ]; then
-       rm .ptplog
+       $SUDO rm .ptplog
 fi
 pids=`pidof ptp4l`
 if [ "$pids" != "" ]; then
        for pid in $pids
        do
-               kill $pid
+               $SUDO kill $pid
        done
 fi
-ptp4l -P -2 -H -i $ptp_interface -m -f $conffile >& .ptplog &
-sleep 60
+$SUDO ptp4l -P -2 -H -i $EMAC0 -m -f $conffile >& .ptplog &
+sleep 30
 tag=$( tail -n 3 .ptplog )
 if [ "$tag" == "" ]; then
        echo "PTP not configured properly"
+       exit
 fi
 echo "PTP log: "
 echo $tag
@@ -114,4 +123,3 @@ inotifywait -m "$datadir" -e create -e moved_to --format "%w%f" |
     while read file; do
        launch_parser $file
     done
-
